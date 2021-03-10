d30963c658edc5d3e3fcc2c82add9401524c38b4
Francis Lagares <francis.lagares@gmail.com>
2021-03-10 21:01:21 +0100

    style: add global styles

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,57 @@
 import React from 'react';
+// import { useStaticQuery, graphql } from 'gatsby';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+import { normalize } from 'styled-normalize';
+
+const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  * {
+    text-decoration: none;
+  }
+
+  html {
+    box-sizing: border-box;
+    -webkit-font-smoothing: antialiased;
+    font-size: 1.6rem;
+  }
+
+  body {
+    font-family: 'Montserrat', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background: ${props => props.theme.background};
+    overscroll-behavior: none;
+    overflow-x: hidden;
+  }
+`;
 
 const Layout: React.FC = ({ children }) => {
-  return <main>{children}</main>;
+  /* const data = useStaticQuery(graphql`
+    query SiteTitleQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `); */
+
+  /* const darkTheme = {
+    background: '#000',
+    text: '#fff',
+  }; */
+
+  const lightTheme = {
+    background: '#fff',
+    text: '#000',
+  };
+
+  return (
+    <>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
+        <main>{children}</main>
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default Layout;
